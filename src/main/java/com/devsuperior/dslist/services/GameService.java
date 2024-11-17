996e0cbd7c4abaa140af8917d0dfdc600cb460c8
Carlos Alexandre <carlos.alexandre.smo@gmail.com>
2024-11-17 18:13:06 -0300

    Domain model

--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -6,9 +6,7 @@ import com.devsuperior.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class GameService {
